04803dc9560c3ff418dcc9518e1c67118cd8f80d
Nader Mohamed <nader.m@salla.sa>
2023-06-18 03:37:16 +0300

    03:37-18-Jun-2023

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>Tasks App</title>
+    <style>
+        label {
+            display: block;
+            font: 1rem 'Fira Sans', sans-serif;
+        }
+
+        input,
+        label {
+            margin: 0.4rem 0;
+        }
+    </style>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 </head>
 <body>
 <div class="container mt-2">
+    <label for="site-search">Search the site:</label>
+    <input type="text" id="site-search" name="q">
+    <button class="searchButton">Search</button>
+
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -59,3 +74,29 @@
 </div>
 </body>
 </html>
+
+{{--add jquery cdn--}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+{{--add script--}}
+{{-- on enter key preessed jquery--}}
+<script>
+    $(document).ready(function(){
+        $("input:text[name=q]").keypress(function(e){
+            if(e.keyCode===13)
+            {
+                let textVal = $("input:text[name=q]").val();
+                let value = "?q=" + textVal;
+                location.href = "{{url('users')}}" + value;
+            }
+        });
+    });
+</script>
+<script>
+    $(".searchButton").click(function(){
+        var textVal = $("input:text[name=q]").val();
+
+        value = "?q=" + textVal;
+        location.href = "{{url('users')}}" + value;
+
+    });
+</script>
