6373adea0235c420f95e971c1ca305528b90532f
Nader Mohamed <nader.m@salla.sa>
2023-07-18 03:03:14 +0300

    03:03 18 Jul 2023

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,16 +22,9 @@ class UserController extends BaseController
         //todo check the load time without the queries
         $users = User::query()
             ->select(['users.*'])
-            ->orderBy(
-                (
-                    Company::query()
-                        ->select('name')
-                        ->whereColumn('id', 'users.company_id')
-                        ->orderBy('name')
-                    ->take(1)
-                )
-            )
+            ->join('companies', 'companies.id', '=', 'users.company_id')
             ->with('company:id,name')
+            ->orderBy('companies.name')
             ->paginate()
         ;
         return view('users.index', ['users' => $users]);
